6bd6d4fb57ddb492c8c276b4a9c53b788bcd6f0f
Icetail1 <51836525+Icetail1@users.noreply.github.com>
2022-11-14 09:46:36 +0900

    Update artifacts.js

--- e7-damage-calc/artifacts.js
+++ e7-damage-calc/artifacts.js
@@ -377,6 +377,6 @@ const artifacts = {
     name: 'Sword Of Adin',
     type: artifactDmgType.damage,
     exclusive: classType.thief,
-    value:()  => (document.getElementById(`elem-adv`).checked ? 0.25 : 0),
+    value: () => 0.25
   }
 };
