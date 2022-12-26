bf79a5502a09374a4499497dedd2f3db6adacb38
Icetail <51836525+Icetail1@users.noreply.github.com>
2022-12-26 10:27:11 +0900

    Update heroes.js

--- e7-damage-calc/heroes.js
+++ e7-damage-calc/heroes.js
@@ -7130,8 +7130,7 @@ const heroes = {
         pow: 1,
         flat: () => elements.caster_defense.value()*0.5,
         flatTip: () => ({ caster_defense: 50 }),
-        extraDmg: () => elements.caster_defense.value()*1.4,
-        extraDmgTip: () => ({ caster_defense: 140}),
+        afterMath: (hitType) => (hitType !== hitTypes.miss) ? { defPercent: 1.4, penetrate: 0 } : null,
         enhance: [0.05, 0.05, 0, 0.1, 0.1],
         single: true,
       }
