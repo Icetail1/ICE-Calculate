22fdf5792fbe9542050c4daa83764db4dbcde02b
Icetail <51836525+Icetail1@users.noreply.github.com>
2023-07-24 10:36:47 +0900

    Update artifacts.js

--- e7-damage-calc/artifacts.js
+++ e7-damage-calc/artifacts.js
@@ -421,7 +421,7 @@ const artifacts = {
     scale:[0.05, 0.055, 0.06, 0.065, 0.07, 0.075, 0.08, 0.085, 0.09, 0.095, 0.1],
     type: artifactDmgType.damage,
     form: [elements.caster_add],
-    value: (artiScale) => artiScale*(elements.caster_add.value() ? 1 : 2),
+    value: (artiScale) => artiScale*(elements.caster_add.value() ? 2 : 1),
     exclusive: classType.warrior,
   },
   sword_of_adin: {
