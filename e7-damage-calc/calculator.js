0fed81067989515a132b54e880362a80b7d8ab82
Icetail <51836525+Icetail1@users.noreply.github.com>
2023-10-20 01:43:01 +0000

    Update calculator.js

--- e7-damage-calc/calculator.js
+++ e7-damage-calc/calculator.js
@@ -132,7 +132,7 @@ const getGlobalAtkMult = () => {
 const getGlobalDamageMult = (hero, skill) => {
   let mult = 0.0;
   const dmgUpBox = document.getElementById('dmg').value;
-  mult += Number(dmgUpBox) * 0.1;
+  mult += Number(dmgUpBox) * 0.01;
   for (let checkboxId of ['rage-set']) {
     const elem = document.getElementById(checkboxId);
     mult += elem.checked ? Number(elem.value)-1 : 0.0;
