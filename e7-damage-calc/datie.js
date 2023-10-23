b55fd777384b02fba95469cdc2d213c10e0fd806
Icetail <51836525+Icetail1@users.noreply.github.com>
2023-10-23 16:31:35 +0900

    Update datie.js

--- e7-damage-calc/datie.js
+++ e7-damage-calc/datie.js
@@ -42,34 +42,34 @@ function random(){
 
 function addThree(){ 
    const choice=Math.floor(Math.random()*4);
-   let num =  Number(document.getElementById('num').value);
+   let num =  Number(document.getElementById('num').innerText);
    switch(choice){
      case 0:
-         num = Number(num) + 3;
+         num = num + 3;
          console.log(num);
-         document.getElementById('num').innerText = num;
-         let atk_after = Number(document.getElementById('atk_after').value);
+         document.getElementById('num').innerText = num.toString();
+         let atk_after = Number(document.getElementById('atk_after').innerText;
          atk_after =  atk_after + 4 + Math.floor(Math.random()*5);
          document.getElementById('atk_after').innerText = atk_after.toString();
          break;
      case 1:
          num = num + 3;
          document.getElementById('num').innerText = num.toString();
-         let critchance_after = Number(document.getElementById('critchance_after').value);
+         let critchance_after = Number(document.getElementById('critchance_after').innerText);
          critchance_after =  critchance_after + 3 + Math.floor(Math.random()*3);
          document.getElementById('critchance_after').innerText = critchance_after.toString();
          break;
      case 2:
          num = num + 3;
          document.getElementById('num').innerText = num.toString();
-         let critdmg_after = Number(document.getElementById('critdmg_after').value);
+         let critdmg_after = Number(document.getElementById('critdmg_after').innerText);
          critdmg_after =  critdmg_after + 4 + Math.floor(Math.random()*4);
          document.getElementById('critdmg_after').innerText = critdmg_after.toString();
          break;
      case 3:
          num = num + 3;
          document.getElementById('num').innerText = num.toString();
-         let spd_after = Number(document.getElementById('spd_after').value);
+         let spd_after = Number(document.getElementById('spd_after').innerText);
          spd_after =  spd_after + 2 + Math.floor(Math.random()*3);
          document.getElementById('spd_after').innerText = spd_after.toString();
          break;
