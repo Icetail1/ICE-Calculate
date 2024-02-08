d103c6600f8611175f2d449822a491072decf42d
Icetail <51836525+Icetail1@users.noreply.github.com>
2024-02-08 18:04:48 +0900

    Update script.js

--- e7-damage-calc/zh/js/script.js
+++ e7-damage-calc/zh/js/script.js
@@ -135,7 +135,7 @@ const store = {
 				? "3" // Desktop default
 				: IS_HEADER
 				? "1.2" //配置文件头默认值(不必是int)
-				: "3", //手机默认
+				: "2", //手机默认
 			wordShell: true, //文字烟花 默认为开启 若不开启可修改为false
 			autoLaunch: true, //自动发射烟花
 			finale: true, //同时放更多烟花
