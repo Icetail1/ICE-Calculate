557a86c44864fe594993d4581e0bed21940f45db
Icetail <51836525+Icetail1@users.noreply.github.com>
2024-10-28 16:01:41 +0900

    Update heroes.js

--- e7-damage-calc/heroes.js
+++ e7-damage-calc/heroes.js
@@ -5824,7 +5824,8 @@ const heroes = {
     classType: classType.mage,
     baseAtk: 1197,
     hpTransAtk: () => elements.caster_max_hp.value()*0.09,
-    dot: [dot.burn,elements.caster_max_hp],
+    dot: [dot.burn],
+    form: [elements.caster_max_hp],
     skills: {
       s1: {
         rate: 1,
